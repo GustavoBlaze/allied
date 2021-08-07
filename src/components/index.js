7952af8b9f65e31ada2d5ff8491674ce6462b10c
GustavoBlaze <gustavoblaze1524@gmail.com>
2021-08-07 17:07:58 -0300

    refactor(components): add component export alias

--- src/components/index.js
+++ src/components/index.js
@@ -5,8 +5,11 @@ export {
   CardDescription,
   CardTitle,
   CardFooter,
+  CardFooterCTA,
 } from './Card';
 export { Container } from './Container';
 export { Stack } from './Stack';
 export { Text, Title } from './Text';
 export { Link } from './Link';
+export { List } from './List';
+export { CheckoutModal } from './CheckoutModal';
