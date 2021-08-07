80592dbb776233185547c3fa08936cdae3a3350c
GustavoBlaze <gustavoblaze1524@gmail.com>
2021-08-07 13:55:30 -0300

    feat(layouts/Home): new layout to list platforms

--- src/layouts/Home/index.jsx
+++ src/layouts/Home/index.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  Text,
+  Card,
+  CardContent,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+  Link,
+} from '~/components';
+import { Title, List } from './styles';
+
+import { usePlatforms } from '~/hooks/usePlatforms';
 
 export default function Home() {
-  return <Container>Home</Container>;
+  const platforms = usePlatforms();
+
+  return (
+    <Container>
+      <Title>Plataformas</Title>
+      <Text>Selecione uma plataforma</Text>
+
+      <List>
+        {platforms.map(({ sku, nome, descricao }) => (
+          <Card as="li" key={sku}>
+            <CardContent>
+              <CardTitle>{nome}</CardTitle>
+              <CardDescription>{descricao}</CardDescription>
+            </CardContent>
+            <CardFooter>
+              <Link href="#" withArrow>
+                Ver detalhes
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </List>
+    </Container>
+  );
 }
--- src/layouts/Home/styles.js
+++ src/layouts/Home/styles.js
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export const Container = styled.main``;
+export const Title = styled.h1`
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+  font-weight: 600;
+  color: var(--text-color);
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin-top: 3.2rem;
+
+  > li + li {
+    margin-top: 1.6rem;
+  }
+`;
