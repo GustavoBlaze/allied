897839576ffae8df2d09be1993d13d629b154822
GustavoBlaze <gustavoblaze1524@gmail.com>
2021-08-07 14:19:12 -0300

    feat(Link): wrap component with Next.js link

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,27 +1,32 @@
 import React from 'react';
+import NextLink from 'next/link';
 import PropTypes from 'prop-types';
 
 import { Container } from './styles';
 import ArrowNext from '~/assets/svg/arrow-next.svg';
 
-function Link({ children, withArrow, ...rest }) {
+function Link({ children, href, withArrow, ...rest }) {
   return (
-    <Container {...rest} withArrow={withArrow}>
-      {children}
+    <NextLink href={href} passHref>
+      <Container {...rest} withArrow={withArrow}>
+        {children}
 
-      {withArrow && <ArrowNext />}
-    </Container>
+        {withArrow && <ArrowNext />}
+      </Container>
+    </NextLink>
   );
 }
 
 Link.defaultProps = {
   children: undefined,
   withArrow: false,
+  href: undefined,
 };
 
 Link.propTypes = {
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
   withArrow: PropTypes.bool,
+  href: PropTypes.string,
 };
 
 export { Link };
