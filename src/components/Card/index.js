e4280f49f1b71356e94368888da0cf337a6a50fc
GustavoBlaze <gustavoblaze1524@gmail.com>
2021-08-07 17:03:02 -0300

    feat(Card): add CardFooterCTA component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -8,6 +8,8 @@ export const Card = styled.div`
   border-radius: var(--border-radius);
   display: flex;
   flex-direction: column;
+  overflow: hidden;
+  position: relative;
 `;
 
 export const CardContent = styled.div`
@@ -26,8 +28,34 @@ export const CardDescription = styled(Text)`
 `;
 
 export const CardFooter = styled.div`
+  font-family: 'Roboto', Arial, sans-serif;
   align-self: stretch;
   display: flex;
-  padding: 1.6rem 2.3em;
+  padding: 1.6rem 2.3rem;
   border-top: 1px solid var(--border-color);
+`;
+
+export const CardFooterCTA = styled(CardFooter).attrs(() => ({
+  as: 'button',
+}))`
+  background: var(--success-color);
+  color: #fff;
+  font-weight: 600;
+  font-size: 1.8rem;
+  line-height: 2.4rem;
+  text-align: center;
+  border: 0;
+  cursor: pointer;
+  justify-content: space-between;
+  align-items: center;
+
+  transition: all 0.35s ease;
+
+  &:hover {
+    filter: brightness(1.05);
+  }
+
+  svg path {
+    fill: #fff;
+  }
 `;
