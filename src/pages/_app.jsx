ca381c3ce49d8c8921c352d910fefe939db0bfbf
GustavoBlaze <gustavoblaze1524@gmail.com>
2021-08-08 22:01:30 -0300

    feat(_app): show loading while is navigating between pages

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,13 @@
 /* eslint-disable react/prop-types */
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
 import GlobalStyle from '~/styles/GlobalStyle';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
