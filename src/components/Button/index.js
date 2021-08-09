f18ca9f6a3841bcad0f7dc90328d34bf0f641c81
GustavoBlaze <gustavoblaze1524@gmail.com>
2021-08-08 21:55:58 -0300

    feat(Button): apply styles

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,23 @@
 import styled from 'styled-components';
 
-export const Button = styled.button``;
+export const Button = styled.button`
+  padding: 1rem 2rem;
+  border-radius: var(--default-border-radius);
+  background: var(--primary-color);
+  font-family: 'Roboto', Arial, sans-serif;
+  border: 0;
+
+  color: #fff;
+  font-size: 1.6rem;
+  line-height: 2.8rem;
+
+  cursor: pointer;
+
+  transition: all 0.35s ease;
+
+  font-weight: 500;
+
+  &:hover {
+    filter: brightness(1.15);
+  }
+`;
