c8edf16d5865758c218f176a9f7f02f21ff8fc3c
GustavoBlaze <gustavoblaze1524@gmail.com>
2021-08-08 22:00:02 -0300

    refactor(List): let list reponsive

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 
 export const List = styled.ul`
-  display: flex;
-  flex-direction: column;
   margin-top: 3.2rem;
 
-  > li + li {
-    margin-top: 1.6rem;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr;
+  grid-gap: 1.6rem;
+
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `;
